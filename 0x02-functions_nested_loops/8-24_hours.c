2adbfd53ac24010c34cc7f64a24453a1d6755eb9
Abdallah <abdallah97158@gmail.com>
2023-09-12 15:16:54 +0300

    2023-09-12|15:16:54

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,13 +9,12 @@ void jack_bauer(void) {
 
     for (hour = 0; hour < 24; hour++) {
         for (minute = 0; minute < 60; minute++) {
-            // Print the hour (0-23) and minute (00-59) in the desired format
-            _putchar((hour / 10) + '0'); // Print the tens digit of the hour
-            _putchar((hour % 10) + '0'); // Print the ones digit of the hour
-            _putchar(':');               // Print the colon separator
-            _putchar((minute / 10) + '0'); // Print the tens digit of the minute
-            _putchar((minute % 10) + '0'); // Print the ones digit of the minute
-            _putchar('\n');              // Print a newline character to move to the next line
+            _putchar((hour / 10) + '0');
+            _putchar((hour % 10) + '0');
+            _putchar(':');
+            _putchar((minute / 10) + '0');
+            _putchar((minute % 10) + '0');
+            _putchar('\n');
         }
     }
 }
