052a612aff951bed3a3c2ae503c6c7b05152915e
Abdallah <abdallah97158@gmail.com>
2023-09-12 14:23:59 +0300

    2023-09-12|14:23:59

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet print alpha from a -> z
+ * Return: void value
+ */
+int print_sign(int n)
+{	
+	if (n > 0)
+		return(1);
+	}
+	if(n == 0)
+	{
+		return(0);
+	}	
+	if (n < 0)
+	{
+		return(-1 * n );
+	}
+	return(0);
+
+}
