10d14b4440bcaf09bdbf41aadcabff2f0d0aea77
Abdallah <abdallah97158@gmail.com>
2023-09-12 14:09:52 +0300

    2023-09-12|14:09:52

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_alphabet print alpha from a -> z
+ * Return: void value
+ */
+int _islower(int c)
+{	
+	int f;
+	
+	f='A';
+	while (f <= 'Z')
+	{
+	
+		int l;
+
+		for (l = 'a'; l <= 'z'; l++)
+		{
+		if (c == l|| c == f)
+		{
+			return(1)
+		}	
+		}
+
+		f++;
+	}
+	return(0);
+
+}
