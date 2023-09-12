418140a01409511492a2bfacf38e7ff762907555
Abdallah <abdallah97158@gmail.com>
2023-09-12 14:00:26 +0300

    2023-09-12|14:00:26

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet print alpha from a -> z
+ * Return: void value
+ */
+int print_alphabet(char c)
+{	
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+	if (c == l)
+	{
+		return(1);
+	}	
+	
+	}
+	return(0);
+
+}
