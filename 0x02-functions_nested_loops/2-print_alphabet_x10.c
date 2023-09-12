8b53247802aea746e881a8a08e2a4c9091fb3e3e
Abdallah <abdallah97158@gmail.com>
2023-09-12 13:36:10 +0300

    2023-09-12|13:36:10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet print alpha from a -> z 10 times
+ * Return: void value
+ */
+void print_alphabet_x10(void)
+{	int l;
+
+	for (l = 0; l <= 10; l++)
+	{
+	print_alphabet()
+	}
+	_putchar('\n');
+	return;
+
+}
