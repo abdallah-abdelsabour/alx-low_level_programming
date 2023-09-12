1a56217e32eaf2f7d54ad20534c9c4cc41e63fdf
Abdallah <abdallah97158@gmail.com>
2023-09-12 14:59:16 +0300

    2023-09-12|14:59:16

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet print alpha from a -> z
+ * Return: void value
+ */
+void _jack_bauer(void c)
+{	
+	int h;
+	
+	for (h = 0; h < 24; h ++ )
+	{
+		int m;
+		for (m = 0; m < 60;m++ )
+		{
+			_putchar(h);
+			_ptchar(':');
+			_putchar(m);
+			_putchar('\n');
+		}
+	}
+}
