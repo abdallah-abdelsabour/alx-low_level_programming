7ff52148a7daa46e7ed86cefda5083b23a0194a2
Abdallah <abdallah97158@gmail.com>
2023-09-18 13:44:58 +0300

    2023-09-18|13:44:58

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * print_rev - putchar in reverse order
+ * @str: pointer to the string
+ * Return: void
+ *
+ *
+ */
+
+void _puts(char *s)
+{
+	int last_char;
+  last_char = 0;
+	while(s[last_char] != '\0')
+	{
+			last_char++;
+	}
+  while(last_char + 1 )
+  {
+      putchar(s[last_char]);
+      last_char--;
+  }
+
+
+}
