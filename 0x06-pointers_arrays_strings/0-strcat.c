7e9794d3dd3cb15e12249ef22ab10ac8471a1dc3
Abdallah <abdallah97158@gmail.com>
2023-09-20 16:10:57 +0300

    2023-09-20|16:10:57

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * _strcat - putchar returns
+ * @dest: pointer to the string
+ * @src: pointer to the string
+ * Return: *char
+ *
+ *
+ */
+
+char *_strcat(char *dest, char *src);
+{
+	int i,k;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[k] != '\0')
+	{
+		dest[i] = src[k];
+		k++;
+		l++;
+	}
+	l++;
+	dest[l] = '\0';
+
+	return(dest);
+}
+
