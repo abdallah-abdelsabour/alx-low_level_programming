902993ad36b279be23cafcc97c1cbb211cbfb489
Abdallah <abdallah97158@gmail.com>
2023-09-25 23:11:09 +0300

    2023-09-25|23:11:09

---
+++ 0-memset.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ * _memset = copy set value
+ * @s: some another thing 
+ * @b: some thing
+ * @i: some thing here 
+ * Return nothing
+ */
+
+void _memset(char *s , char b , int n)
+{
+	int i;
+	
+	while ( n > 0)
+	{
+		s[i++] = b
+	}
+
+}
+
