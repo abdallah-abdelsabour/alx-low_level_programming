f9193007486943377c739bde74ecbc791eacbc81
Abdallah <abdallah97158@gmail.com>
2023-10-04 15:08:57 +0300

    2023-10-04|15:08:57

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * prgname -print program name
+ *
+ *
+ *
+ */
+
+ int main(int argc, char *argv[])
+ {
+	 int i;
+	 for (i =0; i <argc; i++ )
+	 {
+		 printf("%s\n",argv[i]);
+	 }
+ }
+
+
