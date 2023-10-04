5e993c2b380761436292f154a7a9daf5938ddf75
Abdallah <abdallah97158@gmail.com>
2023-10-04 15:18:17 +0300

    2023-10-04|15:18:17

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 /**
- * prgname -print program name
- *
- *
+ * name -print program name
+ * @argc: number of command
+ * @argv: array of argv
+ *Return :  0
  *
  */
 
  int main(int argc, char *argv[])
  {
  	printf("%s\n",argv[0]);
+	return(0);
  }
