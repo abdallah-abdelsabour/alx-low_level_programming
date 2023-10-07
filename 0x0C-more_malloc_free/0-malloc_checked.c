a9e45723e92ea5cfd03218eefe762a633ee127b5
Abdallah <abdallah97158@gmail.com>
2023-10-07 09:31:11 +0300

    2023-10-07|09:31:11

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+* malloc_checked - return memo allocate
+* @b :allocate space 
+* Return : ptr
+*
+ * */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	*ptr = malloc(b);
+	if (!ptr)
+	{
+		exit(98);
+	}	
+}
